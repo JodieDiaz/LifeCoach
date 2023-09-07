83303cc6ee4dceddaae79ccbc3b20f4f69f120cc
JodieDiaz <jodie_diaz@hotmail.com>
2023-09-07 15:57:38 -0500

    limpieza

--- dafne.js
+++
@@ -1,6 +0,0 @@
-
-window.addEventListener("scroll", function () {
-  var scrollTop = window.scrollY || document.documentElement.scrollTop;
-  var parallaxBgCitas = document.querySelector(".parallax-bg-citas");
-  parallaxBgCitas.style.transform = "translateY(" + scrollTop * 0.5 + "px)";
-});
