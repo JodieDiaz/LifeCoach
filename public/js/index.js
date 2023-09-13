17f5eeef12de82e355bc9416faeb1d44e5ff9751
JodieDiaz <jodie_diaz@hotmail.com>
2023-09-12 19:54:46 -0500

    flecha y scroll

--- public/js/index.js
+++ public/js/index.js
@@ -13,3 +13,28 @@ window.addEventListener("scrollimgp", function () {
    parallaxBg.style.transform = `translateY(${scrollY * 0.3}px)`; // Ajusta la velocidad del efecto
  });
 
+
+ document
+   .querySelector('a[href="#about-section"]')
+   .addEventListener("click", function (e) {
+     e.preventDefault();
+     const targetSection = document.querySelector(this.getAttribute("href"));
+
+     if (targetSection) {
+       const offset = targetSection.offsetTop - 100; // Resta 100 píxeles al offset
+
+       const scrollDuration = 5000;
+       const scrollStep = Math.PI / (scrollDuration / 15);
+       let count = 0;
+       const scrollInterval = setInterval(function () {
+         if (window.scrollY !== offset) {
+           count = count + 1;
+           const newOffset =
+             offset - (offset - window.scrollY) * Math.cos(count * scrollStep);
+           window.scrollTo(0, newOffset);
+         } else {
+           clearInterval(scrollInterval);
+         }
+       }, 15);
+     }
+   });
